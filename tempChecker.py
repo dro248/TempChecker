74e5465434f5253c79ed01ccdff219fdc1d3883f
David <david.ostler001@gmail.com>
2016-11-13 08:50:52 -0700

    cuts away extraneous information

--- tempChecker.py
+++ tempChecker.py
@@ -8,6 +8,19 @@ import time
 
 while True:
 	raw_temp_str = subprocess.check_output("sensors", shell=True)
-	print "Raw Temp"
-	print raw_temp_str
+	# print "Raw Temp"
+	startIndex = raw_temp_str.find('Core')
+	temp = raw_temp_str[startIndex:]		
+	
+	startIndex = temp.find('Core')
+	endIndex = temp.find(')') + 1
+
+	temp = temp[startIndex:endIndex]
+	
+	# core0_temp = raw_temp_str[startIndex:endIndex]
+
+	# print core0_temp
+	print temp
+	# print "startIndex:", temp.find('Core')
+	# print "endIndex:", temp.find(')')
 	time.sleep(1)
